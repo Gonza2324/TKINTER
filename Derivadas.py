3237a98516013c878186c71b31c4a57029162d86
Gonzalo Cabada <gonzalocabadaupn@gmail.com>
2021-10-31 17:00:17 -0500

    Aplicacion inicial

---
+++ Derivadas.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from sympy import *
+
+x = Symbol("x")
+
+root = Tk()
+root.title("Derivadas")
+root.geometry("250x100")
+
+funcion = StringVar()
+barra = Entry(root, width=40, textvariable=funcion)
+barra.focus()
+barra.grid(row=0, column=0, columnspan=1)
+
+resultado = StringVar()
+l = Label(root, textvariable=resultado)
+l.grid(row=1, column=0)
+
+def Derivar():
+    pass
+
+btn = Button(root, text="Cacular", command=Derivar)
+btn.grid(row=2, column=0, columnspan=1)
+
+root.mainloop()
