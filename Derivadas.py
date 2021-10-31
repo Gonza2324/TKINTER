5ceeaafc0675e570a771d6c4c0dd93b3c7a5b001
Gonzalo Cabada <gonzalocabadaupn@gmail.com>
2021-10-31 17:05:30 -0500

    Resolvi el orden de la funcion

--- Derivadas.py
+++ Derivadas.py
@@ -14,14 +14,14 @@ barra = Entry(root, width=40, textvariable=funcion)
 barra.focus()
 barra.grid(row=0, column=0, columnspan=1)
 
-
-btn = Button(root, text="Cacular", command=Derivar)
-btn.grid(row=2, column=0, columnspan=1)
-
 def Derivar():
     valor = funcion.get()
     calcular = diff(valor, x)
     resultado.set(calcular)
+
+btn = Button(root, text="Cacular", command=Derivar)
+btn.grid(row=2, column=0, columnspan=1)
+
 
 resultado = StringVar()
 l = Label(root, textvariable=resultado)
