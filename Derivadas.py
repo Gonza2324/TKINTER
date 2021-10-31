7312a212d9d6c2a4dfc7e99ea2f353a7a96e52f2
Gonzalo Cabada <gonzalocabadaupn@gmail.com>
2021-10-31 17:04:00 -0500

    Agregue la funcion y las variables t y q y tambien he reordenado

--- Derivadas.py
+++ Derivadas.py
@@ -2,6 +2,8 @@ from tkinter import *
 from sympy import *
 
 x = Symbol("x")
+t = Symbol("t")
+q = Symbol("q")
 
 root = Tk()
 root.title("Derivadas")
@@ -12,14 +14,18 @@ barra = Entry(root, width=40, textvariable=funcion)
 barra.focus()
 barra.grid(row=0, column=0, columnspan=1)
 
+
+btn = Button(root, text="Cacular", command=Derivar)
+btn.grid(row=2, column=0, columnspan=1)
+
+def Derivar():
+    valor = funcion.get()
+    calcular = diff(valor, x)
+    resultado.set(calcular)
+
 resultado = StringVar()
 l = Label(root, textvariable=resultado)
 l.grid(row=1, column=0)
 
-def Derivar():
-    pass
-
-btn = Button(root, text="Cacular", command=Derivar)
-btn.grid(row=2, column=0, columnspan=1)
 
 root.mainloop()
