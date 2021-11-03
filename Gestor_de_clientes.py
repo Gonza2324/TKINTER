6f3c7d4df3bfbc915af50160620b20d9926cdfed
Gonzalo Cabada <gonzalocabadaupn@gmail.com>
2021-11-03 14:16:21 -0500

    La aplicacion esta terminada

--- Gestor_de_clientes.py
+++ Gestor_de_clientes.py
@@ -20,13 +20,79 @@ cur.execute("""
 conn.commit()
 
 #----------------------BOTONES Y FUNCIONES---------------------
-def nuevo_cliente():
-    pass
+def render_clientes():
+    rows = cur.execute("""SELECT * FROM cliente""").fetchall()
 
+    tree.delete(*tree.get_children())
+
+    for row in rows:
+        tree.insert("", END, row[0], values=(row[1], row[2], row[3]))
+
+def insertar(cliente):
+    cur.execute("""
+                INSERT INTO cliente (nombre, telefono, emprese) VALUES (?, ?, ?)
+                """, (cliente["nombre"], cliente["telefono"], cliente["empresa"]))
+    conn.commit()
+    render_clientes()
+
+def nuevo_cliente():
+    def guardar():
+        if not nombre.get():
+            messagebox.showerror("Error", "El nombre es obligatorio")
+            return
+        if not telefono.get():
+            messagebox.showerror("Error", "El telefono es obligatorio")
+            return
+        if not empresa.get():
+            messagebox.showerror("Error", "La empresa es obligatorio")
+            return
+
+        cliente = {
+            "nombre": nombre.get(),
+            "telefono": telefono.get(),
+            "empresa": empresa.get()
+        }
+        insertar(cliente)
+        top.destroy()
+
+    top = Toplevel()
+    top.title("Nuevo cliente")
+
+    #--------------Nombre--------------
+    lnombre = Label(top, text="Nombre")
+    nombre = Entry(top, width=40)
+    lnombre.grid(row=0, column=0)
+    nombre.grid(row=0, column=1)
+
+    #--------------Telefono----------------
+    ltelefono = Label(top, text="Telefono")
+    telefono = Entry(top, width=40)
+    ltelefono.grid(row=1, column=0)
+    telefono.grid(row=1, column=1)
+
+    #--------------Empresa---------------
+    lempresa = Label(top, text="Empresa")
+    empresa = Entry(top, width=40)
+    lempresa.grid(row=2, column=0)
+    empresa.grid(row=2, column=1)
+
+    #---------------------Guardar-------------------------
+    save = Button(top, text="Guardar", command=guardar)
+    save.grid(row=3, column=1)
+
+    top.mainloop()
 
 def eliminar_cliente():
-    pass
+    id = tree.selection()[0]
 
+    cliente = cur.execute("SELECT * FROM cliente WHERE id = ?", (id, )).fetchone()
+    respuesta = messagebox.askokcancel("¿Seguro?", f"¿Estas seguro de eliminar el cliente {cliente[1]}?")
+    if respuesta:
+        cur.execute("DELETE FROM cliente WHERE id = ?", (id, ))
+        conn.commit()
+        render_clientes()
+    else:
+        pass
 
 btn = Button(root, text="nuevo cliente", command=nuevo_cliente)
 btn.grid(row=0, column=0)
@@ -47,5 +113,7 @@ tree.heading("Telefono", text="Telefono")
 tree.heading("Empresa", text="Empresa")
 
 tree.grid(row=1, column=0, columnspan=2)
+
+render_clientes()
 
 root.mainloop()
