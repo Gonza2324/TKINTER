c798a70f26d1f0f0a83c5c25c233b02b72764751
Gonzalo Cabada <gonzalocabadaupn@gmail.com>
2021-11-03 18:18:46 -0500

    BASE INCIAL DE LA APP

--- Derivadas.py
+++ Derivadas.py
@@ -1,1 +1,18 @@
+from tkinter import *
+from tkinter import ttk
+from sympy import *
+import sqlite3
 
+#-----------BASE DE DATOS------------#
+midb = sqlite3.connect("Derivadas.db")
+cursor = midb.cursor()
+
+#------------------VARIBALES SIMBOLICAS--------------#
+x = Symbol("x")
+y = Symbol("y")
+q = Symbol("q")
+t = Symbol("t")
+
+#----------------------------APLICACION------------------------#
+root = Tk()
+root.title("Derivadas de Funciones Reales de una Sola variable")
