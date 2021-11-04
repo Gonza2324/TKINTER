c063ebd8ac851b8c218ec4ba7b871c019ee68675
Gonzalo Cabada <gonzalocabadaupn@gmail.com>
2021-11-03 23:00:30 -0500

    El esqueleto de la aplicacion esta listo junto con la base de datos

--- Derivadas.py
+++ Derivadas.py
@@ -1,10 +1,16 @@
 from tkinter import *
 from tkinter import ttk
 from sympy import *
-import sqlite3
+import mysql.connector
 
 #-----------BASE DE DATOS------------#
-midb = sqlite3.connect("Derivadas.db")
+midb = mysql.connector.connect(
+    host = "localhost",
+    user = "Gonzalo",
+    password = "KORNkorn2402",
+    database = "derivadas"
+)
+
 cursor = midb.cursor()
 
 #------------------VARIBALES SIMBOLICAS--------------#
@@ -13,6 +19,43 @@ y = Symbol("y")
 q = Symbol("q")
 t = Symbol("t")
 
-#----------------------------APLICACION------------------------#
+#--------------------------------------------------APLICACION----------------------------------------------------------#
 root = Tk()
 root.title("Derivadas de Funciones Reales de una Sola variable")
+def Derivate():
+    funtion_get = funcion.get()
+    calculation = diff(funtion_get, x)
+    str_calculation = str(calculation)
+    answare.set(str_calculation)
+
+    cursor.execute("""INSERT INTO Funciones (funciones, resultados) VALUES(%s, %s)""", (funcion.get(), str_calculation))
+    midb.commit()
+
+    funcion.delete(0, END)
+
+funcion = Entry(root, width=40)
+funcion.focus()
+funcion.grid(row=0, column=0, columnspan=2, sticky="we")
+
+answare = StringVar()
+resultado = Label(root, textvariable=answare)
+resultado. grid(row=1, column= 0, columnspan=2, sticky="we")
+
+btn_calculo = Button(root, text="Calcular", command = Derivate)
+btn_calculo.grid(row=2, column=0, columnspan=2, sticky="we")
+
+tree = ttk.Treeview()
+tree["columns"] = ("Funciones", "Resultados")
+tree.column("#0", width=0, stretch=NO)
+tree.column("Funciones")
+tree.column("Resultados")
+
+tree.heading("Funciones", text="Funciones")
+tree.heading("Resultados", text="Resultados")
+
+tree.grid(row=3, column=0, columnspan=2)
+
+#cursor.execute("""INSERT INTO Funciones (funciones, resultados) VALUES (%s, %s)""", ())
+#midb.commit()
+
+root.mainloop()
